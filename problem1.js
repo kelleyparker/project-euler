49f97311ff55ba642711034d7f22e2b0af5db6f6
Kelley Parker <kelley.parker07@gmail.com>
2016-05-11 13:24:34 -0500

    Solved problem 1
    solved problem 1 and added problem 2 and 3 files without solutions.

---
+++ problem1.js
@@ -0,0 +1,9 @@
+var number = 0;
+
+for (i = 0; i < 1000; i++) {
+	if (i % 3 == 0 || i % 5 == 0) {
+		number = number + i;	
+	}
+}
+
+console.log(number);
---
+++ problem2.js
@@ -0,0 +1,2 @@
+// for http://projecteuler.net/problem=2 via Odin Project [http://www.theodinproject.com/web-development-101/javascript-basics?ref=lnav]
+
