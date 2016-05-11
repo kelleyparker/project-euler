84c7a7f0cbe9a4a15a1f8bd8ef79849d318bdcc5
Kelley Parker <kelley.parker07@gmail.com>
2016-05-11 15:50:55 -0500

    problem 2 work done
    half way thru problem 2

--- problem2.js
+++ problem2.js
@@ -1,2 +1,17 @@
-// for http://projecteuler.net/problem=2 via Odin Project [http://www.theodinproject.com/web-development-101/javascript-basics?ref=lnav]
+var fibArray = [1,2];			// Declare array of all values created via fib. sequence.
+//var fibArrayEvens = [0];	// Declare an array for only even numbers from the fibArray.
 
+var fibSums = fibArray[counter-1] + fibArray[counter-2];
+
+console.log(fibArray[0]);
+console.log(fibArray[1]);
+
+while (fibArray[counter-1] + fibArray[counter-2] < 4000000) {
+	var counter = 2; // Declare variable for each single value in the fibArray; each value in the array is fibArray[counter].
+	fibArray.push(fibArray[counter-1] + fibArray[counter-2]);
+	console.log(fibArray[counter]);
+	counter++;
+}
+
+console.log(fibArray);
+	
