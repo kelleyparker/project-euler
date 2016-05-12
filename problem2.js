b19743376ce4f5762dc7d349bf566720f43ba1e1
Kelley Parker <kelley.parker07@gmail.com>
2016-05-12 07:04:42 -0500

    edit
    edit

--- problem2.js
+++ problem2.js
@@ -1,3 +1,4 @@
+
 var fibArray = [1,2];			// Declare array of all values created via fib. sequence.
 //var fibArrayEvens = [0];	// Declare an array for only even numbers from the fibArray.
 
@@ -8,10 +9,8 @@ console.log(fibArray[1]);
 
 while (fibArray[counter-1] + fibArray[counter-2] < 4000000) {
 	var counter = 2; // Declare variable for each single value in the fibArray; each value in the array is fibArray[counter].
+	var fibSums = fibArray[counter-1] + fibArray[counter-2];
 	fibArray.push(fibArray[counter-1] + fibArray[counter-2]);
 	console.log(fibArray[counter]);
 	counter++;
 }
-
-console.log(fibArray);
-	
